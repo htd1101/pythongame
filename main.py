0be6f3c16f2427898e41397b384506353b0f1a7a
thedaythatlast <74145889+thedaythatlast@users.noreply.github.com>
2020-12-05 08:39:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
 import pygame
+import pygame_menu
+
+pygame.init()
+surface = pygame.display.set_mode((800, 600)) #designer
+
+
+
+
+def start_the_game():
+    menu.clear()
+    menu.add_text_input('Tên người chơi :', default='Player')
+    #menu.add_button('OK',main_game)
+    menu.add_button('Return', main_menu)
+
+
+def help_game():
+    menu.clear()
+    menu.add_button('Return', main_menu)
+
+
+def main_menu():
+    menu.clear()
+    menu.add_button('Play', start_the_game)
+    menu.add_button('Help', help_game)
+    menu.add_button('Quit', pygame_menu.events.EXIT)
+
+
+menu = pygame_menu.Menu(600, 800, 'Welcome',
+                       theme=pygame_menu.themes.THEME_ORANGE) #designer
+main_menu()
+
+
+menu.mainloop(surface)
